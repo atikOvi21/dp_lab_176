8ef23f7650167bd6d622222b66d9eb1ba2051bea
Atik Shahriar Ovi <atikshahriar@iut-dhaka.edu>
2024-11-16 11:54:08 +0600

    Implement main class

--- Lab7/src/main/java/org/example/Main.java
+++ Lab7/src/main/java/org/example/Main.java
@@ -1,8 +1,27 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args)
     {
+        NotificationAggregatorFacade aggregator = new NotificationAggregatorFacade();
 
+
+
+
+        INotificationPlatform facebook = aggregator.SocialMediaList.get(0);
+        INotificationPlatform twitter = aggregator.SocialMediaList.get(1);
+
+        String notificationId = "12345";
+
+
+        aggregator.markNotificationAsRead(notificationId, facebook);
+
+
+        aggregator.markNotificationAsUnread(notificationId, twitter);
+
+
+        aggregator.deleteNotification(notificationId, facebook);
     }
 }
